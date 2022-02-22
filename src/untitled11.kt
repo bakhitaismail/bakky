4ba8450c9b121eca41c24ad6ac15a0e7a6dbd8f7
bakhitaismail <bakhitaismailk@gmail.com>
2022-02-22 14:38:16 +0300

    my first commit

---
+++ src/untitled11.kt
@@ -0,0 +1,16 @@
+fun main() {
+    val names = arrayOf("Juliet Nakayiza", "Tanyasis Mayiana")
+    println(names)
+    val ages = arrayOf(18, 20)
+    println(ages[0])
+    println(ages[1])
+    val phoneNumber = arrayOf("0711476132", "0110625151")
+    println(phoneNumber)
+    val  weight = arrayOf<String>("35kg", "66kg")
+    println(weight[0])
+    println(weight[1])
+    val notKenyan: Boolean = false
+    val kenyan: Boolean = true
+    println(notKenyan)
+    println(kenyan)
+}
