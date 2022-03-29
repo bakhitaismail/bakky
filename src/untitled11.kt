6323b87a8eaec932f820b26d59635f7e4f48e604
misky-jelle <miskyjelle@gmail.com>
2022-03-29 08:58:49 +0300

    changes

--- src/untitled11.kt
+++ src/untitled11.kt
@@ -1,7 +1,7 @@
 fun main() {
-    val names = arrayOf("Juliet Nakayiza", "Tanyasis Mayiana")
+    val names = arrayOf("Misky Yussuf", "Safiya Abukar")
     println(names)
-    val ages = arrayOf(18, 20)
+    val ages = arrayOf(22, 21)
     println(ages[0])
     println(ages[1])
     val phoneNumber = arrayOf("0711476132", "0110625151")
@@ -9,6 +9,7 @@ fun main() {
     val  weight = arrayOf<String>("52kg","70kg", "45kg","66kg")
     println(weight[0])
     println(weight[1])
+<<<<<<< HEAD
     val notKenyan: Boolean = false
     val kenyan: Boolean = true
     println(notKenyan)
@@ -24,4 +25,10 @@ fun enjoyment(){
 var new ="live life"
 println(agenda)
 
+=======
+    val notAmerican : Boolean = false
+    val American: Boolean = true
+    println(notAmerican)
+    println(American)
+>>>>>>> missy
 }
