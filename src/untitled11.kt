ffce8fb4bd6041057ea7abd48c4aafb7befe0513
misky-jelle <miskyjelle@gmail.com>
2022-03-29 09:19:28 +0300

    changes

--- src/untitled11.kt
+++ src/untitled11.kt
@@ -9,26 +9,8 @@ fun main() {
     val  weight = arrayOf<String>("52kg","70kg", "45kg","66kg")
     println(weight[0])
     println(weight[1])
-<<<<<<< HEAD
-    val notKenyan: Boolean = false
-    val kenyan: Boolean = true
-    println(notKenyan)
-    println(kenyan)
-    happy()
-}
-fun happy(){
-    var num = "Rehema"
-    println(num)
-}
-
-fun enjoyment(){
-var new ="live life"
-println(agenda)
-
-=======
     val notAmerican : Boolean = false
     val American: Boolean = true
     println(notAmerican)
     println(American)
->>>>>>> missy
 }
